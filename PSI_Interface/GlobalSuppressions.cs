51c8fdedefa9e58ef5435b15909bf6678462a5e1
alchemistmatt <matthew.monroe@pnnl.gov>
2021-12-22 18:37:26 -0800

    Suppress warning

---
+++ PSI_Interface/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Maintainability", "RCS1138:Add summary to documentation comment.", Justification = "Allow empty summary nodes in this file", Scope = "type", Target = "~T:PSI_Interface.CV.CV.CVID")]
