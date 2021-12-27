4a630f5ae3443def7799c7747fab75243bf7e1ec
alchemistmatt <matthew.monroe@pnnl.gov>
2021-12-27 13:26:01 -0800

    Suppress unit test warnings

---
+++ Interface_Tests/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("General", "RCS1118:Mark local variable as const.", Justification = "<Pending>", Scope = "member", Target = "~M:Interface_Tests.IdentDataTests.MsPathfinderMzidCreation.CreateMzidFile")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed name", Scope = "type", Target = "~T:Interface_Tests.IdentDataTests.mzIdentMLTests.mzIdentMLReadTests")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed name", Scope = "type", Target = "~T:Interface_Tests.IdentDataTests.mzIdentMLTests.mzIdentMLWriteTests")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed name", Scope = "type", Target = "~T:Interface_Tests.MSDataTests.mzMLTests.mzMLReadTests")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed name", Scope = "type", Target = "~T:Interface_Tests.MSDataTests.mzMLTests.mzMLWriteTests")]
