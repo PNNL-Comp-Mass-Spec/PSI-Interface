f01ff94feef89117474e54bfa0d71d9a0cc0f886
alchemistmatt <matthew.monroe@pnnl.gov>
2025-04-08 18:26:30 -0700

    Add unit test that deletes the input file after calling reader.Read()

--- Interface_Tests/MSDataTests/MSDataReadTests.cs
+++ Interface_Tests/MSDataTests/MSDataReadTests.cs
@@ -32,6 +32,59 @@ namespace Interface_Tests.MSDataTests
         #endregion
 
         [Test]
+        [TestCase(@"MzML\QC_Shew_16_01-15f_MPA_02redo_8Nov16_Tiger_16-02-14.mzML", 9293, false)]
+        [TestCase(@"MzML\QC_Shew_16_01-15f_MPA_02redo_8Nov16_Tiger_16-02-14.mzML.gz", 9293, false)]
+        [TestCase(@"MzML\QC_Shew_16_01-15f_MPA_02redo_8Nov16_Tiger_16-02-14.mzML", 9293, true)]
+        [TestCase(@"MzML\QC_Shew_16_01-15f_MPA_02redo_8Nov16_Tiger_16-02-14.mzML.gz", 9293, true)]
+        public void TestDeleteMzMLAfterRead(string inputFileRelativePath, int expectedSpectra, bool useMSDataWrapper)
+        {
+            if (!TestPath.FindInputFile(inputFileRelativePath, out var sourceFile))
+            {
+                Console.WriteLine("File not found: " + inputFileRelativePath);
+                return;
+            }
+
+            var tempDirectory = Path.GetTempPath();
+            var tempFileName = string.Format("{0}_tmp{1}", Path.GetFileNameWithoutExtension(sourceFile.Name), Path.GetExtension(sourceFile.Name));
+            var tempFile = new FileInfo(Path.Combine(tempDirectory, tempFileName));
+
+            try
+            {
+                if (tempFile.Exists)
+                {
+                    tempFile.Delete();
+                }
+
+                sourceFile.CopyTo(tempFile.FullName);
+
+                var reader = new MzMLReader(tempFile.FullName);
+
+                if (useMSDataWrapper)
+                {
+                    // Option 1:
+                    var mzMLData = reader.Read();
+                    Console.WriteLine("Spectrum count reported by MzMLReader: " + mzMLData.run.spectrumList.count);
+                    Assert.AreEqual(expectedSpectra, int.Parse(mzMLData.run.spectrumList.count), "Spectrum count");
+                }
+                else
+                {
+                    // Option 2:
+                    var mzMLData = new MSData(reader.Read());
+                    Console.WriteLine("Spectrum count reported by MSData: " + mzMLData.Run.SpectrumList.Spectra.Count);
+                    Assert.AreEqual(expectedSpectra, mzMLData.Run.SpectrumList.Spectra.Count, "Spectrum count");
+                }
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Exception: {0}", ex.Message);
+            }
+            finally
+            {
+                tempFile.Delete();
+            }
+        }
+
+        [Test]
         [TestCase(@"MzML\QC_Shew_16_01-15f_MPA_02redo_8Nov16_Tiger_16-02-14.mzML", 9293)]
         [TestCase(@"MzML\QC_Shew_16_01-15f_MPA_02redo_8Nov16_Tiger_16-02-14.mzML.gz", 9293)]
         public void TestMzMLRead(string inputFileRelativePath, int expectedSpectra)
