c1443795be4c7bfba6e248b6199729cfaee4be76
alchemistmatt <matthew.monroe@pnnl.gov>
2025-04-09 16:49:39 -0700

    Merge duplicate unit tests

--- Interface_Tests/MSDataTests/mzMLTests/mzMLReadTests.cs
+++ Interface_Tests/MSDataTests/mzMLTests/mzMLReadTests.cs
@@ -33,36 +33,18 @@ namespace Interface_Tests.MSDataTests.mzMLTests
         #endregion
 
         [Test]
-        //[TestCase(@"mzML\VA139IMSMS_noIndex.mzML", 3145)]
-        //[TestCase(@"mzML\VA139IMSMS_noIndex.mzML.gz", 3145)]
+        // [TestCase(@"mzML\VA139IMSMS_noIndex.mzML", 3145)]
+        // [TestCase(@"mzML\VA139IMSMS_noIndex.mzML.gz", 3145)]
+        // [TestCase(@"mzML\VA139IMSMS.mzML", 3145)]
+        // [TestCase(@"mzML\VA139IMSMS_compressed.mzML", 3145)]
+        // [TestCase(@"mzML\VA139IMSMS.mzML.gz", 3145)]
+        // [TestCase(@"mzML\sample1-A_BB2_01_922.mzML", 43574)]
         [TestCase(@"MzML\QC_Shew_16_01-15f_MPA_02redo_8Nov16_Tiger_16-02-14.mzML", 9293)]
         [TestCase(@"MzML\QC_Shew_16_01-15f_MPA_02redo_8Nov16_Tiger_16-02-14.mzML.gz", 9293)]
+        [TestCase(@"MzML\Angiotensin_AllScans.mzML", 1775)]
+        [TestCase(@"MzML\Angiotensin_AllScans_NoIndex.mzML", 1775)]
+
         public void MzMLReadTest(string inputFileRelativePath, int expectedSpectra)
-        {
-            if (!TestPath.FindInputFile(inputFileRelativePath, out var sourceFile))
-            {
-                Console.WriteLine("File not found: " + inputFileRelativePath);
-                return;
-            }
-
-            var reader = new MzMLReader(sourceFile.FullName);
-            var mzMLData = reader.Read();
-
-            Console.WriteLine("Spectrum count: " + mzMLData.run.spectrumList.count);
-            Console.WriteLine("Array length: " + mzMLData.run.spectrumList.spectrum.Count);
-
-            Assert.AreEqual(expectedSpectra.ToString(), mzMLData.run.spectrumList.count, "Spectrum Count");
-            Assert.AreEqual(expectedSpectra, mzMLData.run.spectrumList.spectrum.Count, "Array length");
-        }
-
-        [Test]
-        //[TestCase(@"mzML\VA139IMSMS.mzML", 3145)]
-        //[TestCase(@"mzML\VA139IMSMS_compressed.mzML", 3145)]
-        //[TestCase(@"mzML\VA139IMSMS.mzML.gz", 3145)]
-        //[TestCase(@"mzML\sample1-A_BB2_01_922.mzML", 43574)]
-        [TestCase(@"MzML\QC_Shew_16_01-15f_MPA_02redo_8Nov16_Tiger_16-02-14.mzML", 9293)]
-        [TestCase(@"MzML\QC_Shew_16_01-15f_MPA_02redo_8Nov16_Tiger_16-02-14.mzML.gz", 9293)]
-        public void MzMLIndexedReadTest(string inputFileRelativePath, int expectedSpectra)
         {
             if (!TestPath.FindInputFile(inputFileRelativePath, out var sourceFile))
             {
